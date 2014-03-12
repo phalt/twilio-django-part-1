ea9ee4be5290f0a27dc7471a4096e39d005262f3
Paul Hallett <phalt@users.noreply.github.com>
2014-03-12 18:05:27 +0000

    Update urls.py

--- django1/urls.py
+++ django1/urls.py
@@ -1,8 +1,16 @@
-# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
 
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'django1.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    #Here we add our Twilio URLs
     url(r'^sms/$', 'django1.views.sms'),
     url(r'^ring/$', 'django1.views.ring'),
 )
